edabfc7ab5d181c0a1ad922752645211255164cd
Mulalo12 <133537863+Mulalo12@users.noreply.github.com>
2024-10-24 21:35:36 +0200

    Create Join.cs

---
+++ Testing/Models/Join.cs
@@ -0,0 +1,18 @@
+namespace Testing.Models
+{
+    public class Join
+    {
+        public int JoinID { get; set; }
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public int Age { get; set; }
+        public string? Occupation { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? Address { get; set; }
+        public string? Address2 { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? Zip { get; set; }
+    }
+}
